ada7e7c6436c57d7fdbf8933bfc671c8b02c5f20
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2024-04-21 23:52:51 +0200

    add flake support

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+    };
+    lib = pkgs.lib;
+    version = "0.1.0";
+  in
+  {
+    defaultPackage.x86_64-linux = pkgs.buildGoModule {
+      pname = "nixup";
+      version = version;
+      src = self;
+
+      vendorHash = "sha256-ticfUZwpcvc8G4xpaQ4nApkmpyfnbhMyZu97iB1JGNI=";
+
+      CGO_ENABLED = 0;
+
+      outputs = [ "out" ];
+
+      ldflags = [
+        "-s" "-w" "-X main.version=${version}"
+      ];
+
+      meta = with lib; {
+        homepage = "https://github.com/alvaro17f/nixup";
+        description = "nixup";
+        license = licenses.mit;
+        maintainers = with maintainers; [ alvaro17f ];
+        platforms = platforms.unix;
+        #changelog = "https://github.com/alvaro17f/nixup/blob/${version}/CHANGELOG.md";
+        mainProgram = "nixup";
+      };
+    };
+  };
+}
